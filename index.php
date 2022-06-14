d7e70377079749e270ac3009d78ed16328ecd7e8
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-06-14 14:42:15 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!-- L'e-commerce vende prodotti per gli animali:
+I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti. :carello_della_spesa:
+Il pagamento avviene con la carta di credito, che non deve essere scaduta. -->
+
